4ae69908a6f76dcc3f34cb6c51212f4c89ff94ac
Robert Coleman <github@robert.net.nz>
2014-02-23 08:11:44 +1300

    update x64 install url

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
 ﻿try {
-	$packageName = 'jq'
-	$url = 'http://stedolan.github.io/jq/download/win32/jq.exe'
-	$url64 = 'http://stedolan.github.io/jq/download/win64/jq.exe'
+  $packageName = 'jq'
+  $url = 'http://stedolan.github.io/jq/download/win32/jq.exe'
+  $url64 = 'http://stedolan.github.io/jq/download/win64/jq.exe'
 
-	$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-	$fileFullPath = Join-Path $scriptPath "$packageName.exe"
-	Get-ChocolateyWebFile $packageName $fileFullPath $url $url64bit
-	Install-ChocolateyPath $fileFullPath 'machine'
-	$env:Path += ";$fileFullPath"
-	
-	Write-ChocolateySuccess "$packageName"
+  $scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $fileFullPath = Join-Path $scriptPath "$packageName.exe"
+  Get-ChocolateyWebFile $packageName $fileFullPath $url $url64
+  Install-ChocolateyPath $fileFullPath 'machine'
+  $env:Path += ";$fileFullPath"
+
+  Write-ChocolateySuccess "$packageName"
 } catch {
-	Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-	throw
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
 }
